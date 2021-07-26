686e10380c27f8d5daed0d661c478e27aa67321c
MaximPande <maxpande@gmail.com>
2021-07-25 21:25:32 -0600

    Test
    Test

--- HelloWorld3/Program.cs
+++ HelloWorld3/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World 3 Release");
+            Console.WriteLine("Hello World 4 Release");
             Console.ReadKey();
         }
     }
